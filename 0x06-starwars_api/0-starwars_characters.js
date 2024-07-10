2c95ba77b1a455cd969f4fc4aad9a9cf32a8112a
oovaa <omarvx211@gmail.com>
2024-07-10 14:03:25 +0200

    update it again at 10-14

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,41 @@
 #!/usr/bin/node
 
-import request from 'request';
+const request = require('request');
 
+// Check if a movie ID is provided
+if (process.argv.length < 3) {
+  console.log('Usage: ./script.js <movie_id>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+
+// Fetch the movie details
 request(
-  'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  'https://swapi.dev/api/films/' + movieId + '/',
   function (err, res, body) {
     if (err) throw err;
+
+    // Parse the response body
     const actors = JSON.parse(body).characters;
+
+    // Function to print characters in exact order
+    const exactOrder = (actors, index) => {
+      if (index === actors.length) return;
+
+      // Fetch each character details
+      request(actors[index], function (err, res, body) {
+        if (err) throw err;
+
+        // Print the character name
+        console.log(JSON.parse(body).name);
+
+        // Recursive call to process the next character
+        exactOrder(actors, index + 1);
+      });
+    };
+
+    // Start the process with the first character
     exactOrder(actors, 0);
   }
 );
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
--- 0x06-starwars_api/README.md
+++ 0x06-starwars_api/README.md
@@ -1,1 +1,32 @@
-0x06. Star Wars API
+```markdown
+# Star Wars Characters Script
+
+## Description
+This script prints all characters of a specified Star Wars movie using the Star Wars API.
+
+## Usage
+Make the script executable and run it from the command line, passing the movie ID as an argument:
+```bash
+chmod +x script.js
+./script.js <movie_id>
+```
+
+## Requirements
+- Ubuntu 20.04 LTS
+- Node.js 10.14.x
+- Request module
+```
+
+### Make the script executable
+Ensure the script is executable:
+```bash
+chmod +x script.js
+```
+
+### Test the length of the file
+Test the length of the file using `wc`:
+```bash
+wc -l script.js
+```
+
+This script should now meet all the requirements you listed.
